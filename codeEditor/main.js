99b1cdd70f1a05da8a9199d17bbfff4693714700
MarioSC21 <mariocamna@gmail.com>
2022-05-24 12:58:15 -0500

    editor con guardado en url

--- codeEditor/main.js
+++ codeEditor/main.js
@@ -1,6 +1,19 @@
 import './style.css'
+import Split from 'split-grid'
+import { encode, decode } from 'js-base64';
 
 const getElemento = (selector) => document.querySelector(selector);
+
+Split({
+    columnGutters: [{
+        track: 1,
+        element: document.querySelector('.gutter-col-1'),
+    }],
+    rowGutters: [{
+        track: 1,
+        element: document.querySelector('.gutter-row-1'),
+    }]
+})
 
 const $js = getElemento('#js');
 const $css = getElemento('#css');
@@ -12,17 +25,36 @@ $css.addEventListener('input', update)
 
 $html.addEventListener('input', update)
 
-function update () {
-  const html = createHtml()
-  // console.log(html)
-  getElemento('iframe').setAttribute('srcdoc', html)
-  console.log(getElemento('iframe'))
+function init(){
+  const { pathname } = window.location
+
+  const [rawhtml,rawcss,rawjs] =pathname.slice(1).replace('%7D',"").split('%7C')
+  // console.log(pathname.slice(1).split('%7C'))
+
+  const html = `${decode(rawhtml)}`
+  const css = `${decode(rawcss)}`
+  const js = `${decode(rawjs)}`
+  $html.value = html
+  $css.value = css
+  $js.value = js
+
+  const html2 = createHtml({html,css,js})
+  getElemento('iframe').setAttribute('srcdoc', html2)
 }
 
-const createHtml = () => {
+function update () {
   const html = $html.value
   const css = $css.value
   const js = $js.value
+
+  const hashedCode = `${encode(html)}|${encode(css)}|${encode(js)}}`
+  window.history.replaceState(null, null, `/${hashedCode}`)
+
+  const html2 = createHtml({html,js,css})
+  getElemento('iframe').setAttribute('srcdoc', html2)
+}
+
+const createHtml = ({html,js,css}) => {
   const htmlRender = 
   ` <!DOCTYPE html>
     <html lang="en">
@@ -41,3 +73,4 @@ const createHtml = () => {
   return htmlRender
    
 }
+init()
